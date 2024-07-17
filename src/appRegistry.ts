00d5d34eba9873be9b6594632302f9bfdf15adcd
irubido <ivan.rubido@gmail.com>
2024-07-17 13:47:12 +0200

    AppRegistry boilerplate

--- src/appRegistry.ts
+++ src/appRegistry.ts
@@ -1,3 +1,24 @@
-export class AppRegistry {
+import { Contract, ContractInterface, providers } from "ethers";
+import { getGatewayResponse } from "./utils";
 
+export class AppRegistry {
+  private appRegistryContractAddress: string
+  private appRegistryContract: Contract
+  private appRegistryContractABI: ContractInterface
+
+  constructor(provider: providers.BaseProvider) {
+    this.appRegistryContract = new Contract(this.appRegistryContractAddress, this.appRegistryContractABI, provider)
+  }
+
+
+  public async grantPoints(appName: string, operator: string, gatewayName: string) {
+
+    const gatewayResponse = await getGatewayResponse(gatewayName);
+
+    return await this.appRegistryContract.register({
+      appName,
+      operator,
+      gatewayResponse
+      })
+  }
 }
