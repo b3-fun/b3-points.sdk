440502613e228ab276d355fc5ae1da18b7de760c
Qi Wu <wqi@umich.edu>
2024-08-22 10:42:20 +0800

    Add export

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ export { AppRegistry } from "./appRegistry";
 export { 
   SepoliaCCIPGatewayBaseUrl, 
   SepoliaENSResolverAddress,
+  MainnetCCIPGatewayBaseUrl,
+  MainnetENSResolverAddress,
   B3SepoliaPointIndexerURL,
   B3SepoliaAppRegistryContractAddress,
   B3SepoliaPointServiceContractAddress,
   B3MainnetPointIndexerURL,
   B3MainnetAppRegistryContractAddress,
   B3MainnetPointServiceContractAddress,
- } from "./constants";
+} from "./constants";
