e095938f1bf71d816ccd085037f0688a33e192de
0x57 <wqi@umich.edu>
2024-08-22 10:43:01 +0800

    Fix ens lookup (#11)
    * Add constants for mainnet
    * Fix util for ens lookup
    * Fix register
    * Add export
    * Bump version

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,14 @@
+// ENS
 export const SepoliaCCIPGatewayBaseUrl =
   "https://ens-gateway.0x5700.workers.dev/lookup";
 export const SepoliaENSResolverAddress =
   "0x1483219E707786c7E52Ed84d3Ad5C2C468E2fE4A";
+export const MainnetCCIPGatewayBaseUrl =
+  "https://ens-gateway.b3.fun/lookup";
+export const MainnetENSResolverAddress =
+  "0xD940a6a304775e847fac8E9233EF5eda481433f3";
+
+// BPS contracts
 export const B3SepoliaAppRegistryContractAddress =
   "0xa6f674D1DbAfFebd1c0cebbBA7178Ef5Adf829A5";
 export const B3SepoliaPointServiceContractAddress =
@@ -10,6 +17,8 @@ export const B3MainnetAppRegistryContractAddress =
   "0xa6f674D1DbAfFebd1c0cebbBA7178Ef5Adf829A5";
 export const B3MainnetPointServiceContractAddress =
   "0xe16AA715Ac4506Ebd25408952dA2cE56461C3907";
+
+// Points Indexers
 export const B3SepoliaPointIndexerURL =
   "https://b3-fun.squids.live/points-b3-sepolia/graphql";
 export const B3MainnetPointIndexerURL =
