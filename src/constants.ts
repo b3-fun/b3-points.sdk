ea7071643848805f4426701a7eaaabfe8bea783b
0x57 <wqi@umich.edu>
2024-08-24 13:52:06 +0800

    Bring back changes (#13)
    * Dist (#8)
    * Use yarn
    * Ensure to include type declarations in package.json
    * 0.0.3 (#9)
    * Fix typo
    * Update version
    * Export constants (#10)
    * v0.0.4
    * v0.0.5
    * Fix ens lookup (#11)
    * Add constants for mainnet
    * Fix util for ens lookup
    * Fix register
    * Add export
    * Bump version
    * Fix maiinet bps contract addresses
    * Delete package-lock.json

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,24 @@
+// ENS
 export const SepoliaCCIPGatewayBaseUrl =
   "https://ens-gateway.0x5700.workers.dev/lookup";
 export const SepoliaENSResolverAddress =
   "0x1483219E707786c7E52Ed84d3Ad5C2C468E2fE4A";
+export const MainnetCCIPGatewayBaseUrl =
+  "https://ens-gateway.b3.fun/lookup";
+export const MainnetENSResolverAddress =
+  "0xD940a6a304775e847fac8E9233EF5eda481433f3";
+
+// BPS contracts
 export const B3SepoliaAppRegistryContractAddress =
   "0xa6f674D1DbAfFebd1c0cebbBA7178Ef5Adf829A5";
 export const B3SepoliaPointServiceContractAddress =
   "0xe16AA715Ac4506Ebd25408952dA2cE56461C3907";
 export const B3MainnetAppRegistryContractAddress =
-  "0xa6f674D1DbAfFebd1c0cebbBA7178Ef5Adf829A5";
+  "0x28F8EBc9cdD1AB17056Baf306D5b61d2D022cbC6";
 export const B3MainnetPointServiceContractAddress =
-  "0xe16AA715Ac4506Ebd25408952dA2cE56461C3907";
+  "0x5Bcd76f3d818770e31E7a4B6D449e35de0eA3e4D";
+
+// Points Indexers
 export const B3SepoliaPointIndexerURL =
   "https://b3-fun.squids.live/points-b3-sepolia/graphql";
 export const B3MainnetPointIndexerURL =
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 export { BPS } from "./bps";
 export { AppRegistry } from "./appRegistry";
+export { 
+  SepoliaCCIPGatewayBaseUrl, 
+  SepoliaENSResolverAddress,
+  MainnetCCIPGatewayBaseUrl,
+  MainnetENSResolverAddress,
+  B3SepoliaPointIndexerURL,
+  B3SepoliaAppRegistryContractAddress,
+  B3SepoliaPointServiceContractAddress,
+  B3MainnetPointIndexerURL,
+  B3MainnetAppRegistryContractAddress,
+  B3MainnetPointServiceContractAddress,
+} from "./constants";
